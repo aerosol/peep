bb6c1bdf77e36e698940e5385057ce430a89aa63
Adam Rutkowski <hq@mtod.org>
2025-05-08 06:36:41 +0200

    Update bench

--- test/benchmark/get_all_metrics.exs
+++ test/benchmark/get_all_metrics.exs
@@ -1,31 +1,55 @@
+defmodule Fixture do
+  def sample_metrics,
+    do: [
+      Telemetry.Metrics.counter("benchmark.counter", description: "counter"),
+      Telemetry.Metrics.sum("benchmark.sum", description: "sum"),
+      Telemetry.Metrics.last_value("benchmark.last_value", description: "last_value"),
+      Telemetry.Metrics.distribution("benchmark.distribution", description: "distribution")
+    ]
+end
+
 Benchee.run(
   %{
-    "default_dump" => fn tids -> Peep.Storage.Striped.get_all_metrics(tids) end,
-    "new_dump" => fn tids -> Peep.Storage.Striped2.get_all_metrics(tids) end
+    "default_dump" => fn {tids, _} -> Peep.Storage.Striped.get_all_metrics(tids) end,
+    "new_dump" => fn {tids, _} -> Peep.Storage.Striped2.get_all_metrics(tids) end
+  },
+  inputs: %{
+    "small" =>
+      {Peep.Storage.Striped.new(),
+       Enum.map(1..100, fn _ ->
+         [
+           Enum.random(Fixture.sample_metrics()),
+           Enum.random(10..1000),
+           %{foo: Enum.random(1..10), bar: :baz}
+         ]
+       end)},
+    "medium" =>
+      {Peep.Storage.Striped.new(),
+       Enum.map(1..10_000, fn _ ->
+         [
+           Enum.random(Fixture.sample_metrics()),
+           Enum.random(10..1000),
+           %{foo: Enum.random(1..10), bar: :baz}
+         ]
+       end)},
+    "large" =>
+      {Peep.Storage.Striped.new(),
+       Enum.map(1..150_000, fn _ ->
+         [
+           Enum.random(Fixture.sample_metrics()),
+           Enum.random(10..1000),
+           %{foo: Enum.random(1..10), bar: :baz}
+         ]
+       end)}
   },
   # profile_after: true,
-  before_scenario: fn _ ->
-    tids = Peep.Storage.Striped.new()
-
-    sample_metrics =
-      [
-        Telemetry.Metrics.counter("benchmark.counter", description: "counter"),
-        Telemetry.Metrics.sum("benchmark.sum", description: "sum"),
-        Telemetry.Metrics.last_value("benchmark.last_value", description: "last_value"),
-        Telemetry.Metrics.distribution("benchmark.distribution", description: "distribution")
-      ]
-
-    1..150_000
-    |> Task.async_stream(fn _ ->
-      Peep.Storage.Striped.insert_metric(
-        tids,
-        Enum.random(sample_metrics),
-        Enum.random(10..1000),
-        %{foo: Enum.random(1..10), bar: :baz}
-      )
+  before_scenario: fn {tids, metrics} ->
+    metrics
+    |> Task.async_stream(fn [metric, value, tags] ->
+      Peep.Storage.Striped.insert_metric(tids, metric, value, tags)
     end)
     |> Stream.run()
 
-    tids
+    {tids, metrics}
   end
 )
